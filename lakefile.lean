7c160b0b0a9e125fab890a4ba7c5abc61987f9ea
de Medeiros <markusjd@amazon.com>
2024-05-29 11:58:28 -0400

    install doc-gen

--- lakefile.lean
+++ lakefile.lean
@@ -10,3 +10,7 @@ lean_lib «TestProject» where
 @[default_target]
 lean_exe «testproject» where
   root := `Main
+
+-- From doc-gen4 
+meta if get_config? env = some "doc" then 
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
